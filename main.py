f95a8906a3b88a507395182a0716b2ccade543eb
Sujit <sujit.kundu@gmail.com>
2022-05-12 18:21:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,38 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import azure.cognitiveservices.speech as speechsdk
+from flask import *
 
 subscription_key = 'c8e3d272c8a14b4a9b59c3fe18257c89'
 default_region = 'eastus'
+audio_filePath = 'D:/Sujit/hackathon/'
+app= Flask(__name__)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-def default_speech(text, lang='en-US', gender='JennyNeural'):
+@app.route('/speech/default',methods=['POST'])
+def default_speech():#sid, text, lang='en-US', gender='M'
+    #Add validation
+    content= request.get_json(silent=True)
+    sid = content['sid']
+    print(sid)
+    text = content['text']
+    iso_country = content['iso_country']
+    iso_country = "US" if not content['iso_country'] else content['iso_country']
+    lang = 'en' if not content['lang'] else content['lang']
+    gender = 'M' if content['gender'] is None else content['gender']
     speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=default_region)
+    default_voice_M = 'JasonNeural'
 
-
-    audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    #audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    audio_callback_path=audio_filePath+sid+"-def-file.wav";
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=audio_callback_path)
 
     # The language of the voice that speaks.
-    speech_config.speech_synthesis_voice_name = lang+'-'+gender
+    speaker = default_voice_M if gender=='M' else 'NancyNeural'
+    speech_config.speech_synthesis_voice_name = lang+ "-" + iso_country + '-' + speaker
 
     speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
@@ -29,21 +44,49 @@ def default_speech(text, lang='en-US', gender='JennyNeural'):
     #text = input(name)
 
     speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
-
+    callback = {"result": '', "callback_url": ''}
     if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
         print("Speech synthesized for text [{}]".format(text))
+        callback = {"result": 'success',"callback_url": audio_callback_path}
     elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+
         cancellation_details = speech_synthesis_result.cancellation_details
+        callback = {"result": cancellation_details.error_details ,
+                    "callback_url":''
+                    }
+
         print("Speech synthesis canceled: {}".format(cancellation_details.reason))
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
             if cancellation_details.error_details:
                 print("Error details: {}".format(cancellation_details.error_details))
                 print("Did you set the speech resource key and region values?")
+    print("Callback URL: "+ audio_callback_path)
+    json_summary = jsonify(callback)
+    response = make_response(json_summary,200)
+    return response
+
+@app.route('/speech/voice',methods=['POST'])
+def record_speech(sid, text,audio_file_path):
+    # Add validation
+    content = request.get_json(silent=True)
+    sid = content['sid']
+    print(sid)
+    text = content['text']
+    audio_file_path=request.values.get('audio_file_path')
+
+    # audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    audio_callback_path = audio_file_path #audio_filePath + sid + "-def-file.wav";
+
+    callback = {"callback_url": audio_callback_path}
+
+    json_summary = jsonify(callback)
+    response = make_response(json_summary, 200)
+    return response
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    default_speech('Sujit')
+    print_hi('Starting speech api service..')
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
