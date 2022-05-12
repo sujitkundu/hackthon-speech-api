c479d7e62cb1de6399079cb8f9f8f8a3c09f7158
Sujit <sujit.kundu@gmail.com>
2022-05-12 23:49:25 +0530

    yugadb connect
    db connection

--- main.py
+++ main.py
@@ -4,16 +4,31 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import azure.cognitiveservices.speech as speechsdk
-from flask import *
+from flask import g
+#from flask import request,make_response,Flask,jsonify,appcontext_tearing_down,request_started
+import psycopg2
+import asyncio
+#from sqlalchemy import create_engine
+import pandas
+from flask_sqlalchemy import SQLAlchemy
+from models import NameSpeech
+from config import *
+from models import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+import datetime
+from sqlalchemy.sql import func
 
-subscription_key = 'c8e3d272c8a14b4a9b59c3fe18257c89'
-default_region = 'eastus'
-audio_filePath = 'D:/Sujit/hackathon/'
-app= Flask(__name__)
+#DB_URL="host=20.127.242.100 port=5433 dbname=yugabyte user=yugabyte password=Hackathon22!"
+#insert_sql= "INSERT INTO name_pronunciation_details (user_id, first_name, last_name, short_name,voice_path,created_timestamp) VALUES (%s, %s, %s, %s,%s,CURRENT_TIMESTAMP)";
+#migrate = Migrate(app, db)
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+engine_nf = create_engine('postgresql://yugabyte:Hackathon22!@20.127.242.100:5433/yugabyte')
+#sql_read = lambda sql: pd.read_sql(sql, engine_nf)
+#sql_execute = lambda sql: pd.io.sql.execute(sql, engine_nf)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Base.metadata.create_all(engine_nf)
 
 @app.route('/speech/default',methods=['POST'])
 def default_speech():#sid, text, lang='en-US', gender='M'
@@ -48,6 +63,19 @@ def default_speech():#sid, text, lang='en-US', gender='M'
     if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
         print("Speech synthesized for text [{}]".format(text))
         callback = {"result": 'success',"callback_url": audio_callback_path}
+        data_insert = NameSpeech(
+            userID=sid,
+            firstName=text,
+            lastName=text,
+            shortName=text,
+            voicePath=audio_callback_path,
+            created=func.now(),
+        )
+        #Session = sessionmaker(bind=engine_nf)
+
+        db.session.add(data_insert)
+        db.session.commit()
+        print('Saved to DB')
     elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
 
         cancellation_details = speech_synthesis_result.cancellation_details
@@ -60,6 +88,7 @@ def default_speech():#sid, text, lang='en-US', gender='M'
             if cancellation_details.error_details:
                 print("Error details: {}".format(cancellation_details.error_details))
                 print("Did you set the speech resource key and region values?")
+
     print("Callback URL: "+ audio_callback_path)
     json_summary = jsonify(callback)
     response = make_response(json_summary,200)
@@ -86,7 +115,7 @@ def record_speech(sid, text,audio_file_path):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Starting speech api service..')
+    print('Starting speech api service..')
     app.run(host="0.0.0.0", port=8080, debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
