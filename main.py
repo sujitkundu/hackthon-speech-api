5014754f226dc19be690bc0f38e7d18e45a35bbe
Aditya Mangla <aadimangla@gmail.com>
2022-05-14 15:14:05 +0530

    CORS Update

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+from flask import abort, Response
+import json
+import gunicorn
+import os
+from flask import send_file
+import socket
+import traceback
 import base64
 
 import azure.cognitiveservices.speech as speechsdk
@@ -20,55 +29,49 @@ from sqlalchemy import or_
 import logging
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-import traceback
-import socket
-from flask import send_file
-import os
-import gunicorn
-import json
-from flask import abort, Response
-
 
 
 #DB_URL="host=20.127.242.100 port=5433 dbname=yugabyte user=yugabyte password=Hackathon22!"
 #insert_sql= "INSERT INTO name_pronunciation_details (user_id, first_name, last_name, short_name,voice_path,created_timestamp) VALUES (%s, %s, %s, %s,%s,CURRENT_TIMESTAMP)";
 #migrate = Migrate(app, db)
-from sqlalchemy import create_engine
-from sqlalchemy import MetaData
 #engine_nf = create_engine('postgresql://yugabyte:Hackathon22!@20.127.242.100:5433/yugabyte')
 #sql_read = lambda sql: pd.read_sql(sql, engine_nf)
 #sql_execute = lambda sql: pd.io.sql.execute(sql, engine_nf)
 
-#Base.metadata.create_all(engine_nf)
+# Base.metadata.create_all(engine_nf)
 
 audio_filePath = '/home/site/wwwroot/' if platform.system() != 'Windows' else 'D:/Sujit/hackathon/'
 
 app.config['UPLOAD_FOLDER'] = audio_filePath
-default_port=8000
-azure_host="https://checkops.azurewebsites.net"
+default_port = 8000
+azure_host = "https://checkops.azurewebsites.net"
 
 print(audio_filePath)
 print(socket.gethostname())
+
 
 @app.route('/ping')
 def index():
     return "Speech api is running"
 
+
 def audio_path(file_name):
-    full_path= audio_filePath+file_name
+    full_path = audio_filePath+file_name
 
     sample_string_bytes = full_path.encode("ascii")
     base64_bytes = base64.b64encode(sample_string_bytes)
     base64_string = base64_bytes.decode("ascii")
-    get_host_port = "http://"+socket.gethostname() + ":" + str(default_port) if platform.system() == 'Windows' else azure_host
+    get_host_port = "http://"+socket.gethostname() + ":" + \
+        str(default_port) if platform.system() == 'Windows' else azure_host
 
-    return get_host_port+"/download/audio?q="+base64_string;
+    return get_host_port+"/download/audio?q="+base64_string
 
-@app.route('/speech/create',methods=['POST'])
-def default_speech_save_update():#sid, text, lang='en-US', gender='M'
-    #Add validation
+
+@app.route('/speech/create', methods=['POST'])
+def default_speech_save_update():  # sid, text, lang='en-US', gender='M'
+    # Add validation
     print('Adding new...')
-    content= request.get_json()
+    content = request.get_json()
     sid = content['sid']
     f_name = content['firstName']
     l_name = content['lastName']
@@ -79,19 +82,23 @@ def default_speech_save_update():#sid, text, lang='en-US', gender='M'
     iso_country = "US" if not content['iso_country'] else content['iso_country']
     lang = 'en' if not content['lang'] else content['lang']
     gender = 'M' if content['gender'] is None else content['gender']
-    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=default_region)
+    speech_config = speechsdk.SpeechConfig(
+        subscription=subscription_key, region=default_region)
     default_voice_M = 'JasonNeural'
 
     #audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-    file_name = sid+"-def-file.wav";
-    audio_callback_path=audio_filePath+file_name;
-    audio_config = speechsdk.audio.AudioOutputConfig(filename=audio_callback_path)
+    file_name = sid+"-def-file.wav"
+    audio_callback_path = audio_filePath+file_name
+    audio_config = speechsdk.audio.AudioOutputConfig(
+        filename=audio_callback_path)
 
     # The language of the voice that speaks.
-    speaker = default_voice_M if gender=='M' else 'NancyNeural'
-    speech_config.speech_synthesis_voice_name = lang+ "-" + iso_country + '-' + speaker
+    speaker = default_voice_M if gender == 'M' else 'NancyNeural'
+    speech_config.speech_synthesis_voice_name = lang + \
+        "-" + iso_country + '-' + speaker
 
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    speech_synthesizer = speechsdk.SpeechSynthesizer(
+        speech_config=speech_config, audio_config=audio_config)
 
     # Get text from the console and synthesize to the default speaker.
     #print("Enter some text that you want to speak >")
@@ -115,7 +122,8 @@ def default_speech_save_update():#sid, text, lang='en-US', gender='M'
         if not record:
             db.session.add(data_insert)
             db.session.commit()
-            callback = {"result": 'success', "callback_url": audio_path(file_name), "sid":sid, "firstName": f_name, "lastName": l_name, "shortName":s_name, "custVoicePath":''}
+            callback = {"result": 'success', "callback_url": audio_path(
+                file_name), "sid": sid, "firstName": f_name, "lastName": l_name, "shortName": s_name, "custVoicePath": ''}
             print('Saved to DB')
         else:
             setattr(record, 'voice_path', audio_path(file_name))
@@ -126,23 +134,25 @@ def default_speech_save_update():#sid, text, lang='en-US', gender='M'
     elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
 
         cancellation_details = speech_synthesis_result.cancellation_details
-        callback = {"result": cancellation_details.error_details ,
-                    "callback_url":''
+        callback = {"result": cancellation_details.error_details,
+                    "callback_url": ''
                     }
 
-        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        print("Speech synthesis canceled: {}".format(
+            cancellation_details.reason))
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
             if cancellation_details.error_details:
-                print("Error details: {}".format(cancellation_details.error_details))
+                print("Error details: {}".format(
+                    cancellation_details.error_details))
                 print("Did you set the speech resource key and region values?")
 
-    print("Callback URL: "+ audio_callback_path)
+    print("Callback URL: " + audio_callback_path)
     json_summary = jsonify(callback)
-    response = make_response(json_summary,201)
+    response = make_response(json_summary, 201)
     return response
 
 
-@app.route('/speech/update',methods=['PUT'])
+@app.route('/speech/update', methods=['PUT'])
 def record_speech():
     # Add validation
 
@@ -184,7 +194,9 @@ def record_speech():
 def view_all_profiles():
     if request.method == "GET":
         items = NameSpeech.query.all()
-        return jsonify([item.serialize for item in items])
+        resp = jsonify([item.serialize for item in items])
+        resp.headers.add("Access-Control-Allow-Origin", "*")
+        return resp
     else:
         return {"message": "failure"}
 
@@ -202,11 +214,17 @@ def search_profile(text):
             #query_sql = """SELECT * FROM table WHERE user_id LIKE '%' :text '%' or first_name LIKE '%' :text '%' """
             # db is sqlalchemy session object
             #items = db.session.execute(text(query_sql), {"text": text}).fetchall()
-            items = db.session.query(NameSpeech).filter(or_(NameSpeech.first_name.like(search), NameSpeech.user_id.like(search)))
-            return jsonify([item.serialize for item in items])
+            items = db.session.query(NameSpeech).filter(
+                or_(NameSpeech.first_name.like(search), NameSpeech.user_id.like(search)))
+            # return jsonify([item.serialize for item in items])
+            resp = jsonify([item.serialize for item in items])
+            resp.headers.add("Access-Control-Allow-Origin", "*")
+            return resp
         except:
             traceback.print_exc()
-            return jsonify({"error": f"Item {text} not found"})
+            resp = jsonify({"error": f"Item {text} not found"})
+            resp.headers.add("Access-Control-Allow-Origin", "*")
+            return resp
     else:
         return {"message": "Request method not implemented"}
 
@@ -214,12 +232,13 @@ def search_profile(text):
 @app.route("/download/audio", methods=["GET"])
 def download_audio():
     print("File Path-> "+request.args.get('q'))
-    encoded_b64=request.args.get('q');
+    encoded_b64 = request.args.get('q')
     base64_bytes = encoded_b64.encode("ascii")
     sample_string_bytes = base64.b64decode(base64_bytes)
     path = sample_string_bytes.decode("ascii")
     print("Decode Path-> " + path)
     return send_file(path, as_attachment=True)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
